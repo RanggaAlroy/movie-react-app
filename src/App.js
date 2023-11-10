8f462725a913362b3a457e98495228473e9650a0
RanggaAlroy <114912822+RanggaAlroy@users.noreply.github.com>
2023-11-10 07:15:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import MovieCard from './MovieCard';
 
 const API_URL = 'https://www.omdbapi.com?apikey=eec98347';
 
-const movie1 = {
-  Title: 'Spiderman in Cannes',
-  Year: '2016',
-  imdbID: 'tt5978586',
-  Type: 'movie',
-  Poster:
-    'https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg',
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState([]);
